680d66ded9ecd5c876dc484fdec509d7eba6f02d
ASW206 <aswinabishek00@gmail.com>
2025-10-31 17:54:12 +0000

    Fixed controllers

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "da55goplw",
-  api_key: process.env.CLOUDINARY_API_KEY || "711133234327316",
-  api_secret:
-    process.env.CLOUDINARY_API_SECRET || "m3UQNKEd-zcfjyUsZF4zeaKUxW0",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export { cloudinary };
